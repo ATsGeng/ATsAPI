af8ad0f3e54d9adf5ab355faaa813d9ab51d03ff
ahyarrr12 <mustafal.haq@digitalbdg.ac.id>
2026-04-20 15:34:06 +0700

    Refactor API service to use named import for croot
    Updated import statement to use named import for croot library and modified fetchOrgData function to call croot.get.

--- api-service.js
+++ api-service.js
@@ -1,6 +1,11 @@
-import api from "https://cdn.jsdelivr.net/gh/crootjs/lib@0.0.3/api.js";
+// api-service.js
+// Menggunakan * as croot karena library tidak punya default export
+import * as croot from "https://cdn.jsdelivr.net/gh/crootjs/lib@0.0.3/api.js";
 
 export const fetchOrgData = (orgName, targetId) => {
     const url = `https://api.github.com/orgs/${orgName}`;
-    api.get(url, targetId);
+    
+    // Di library v0.0.3, fungsi get dipanggil dari objek hasil import
+    // Parameter: (URL, ID_Target)
+    croot.get(url, targetId);
 };
