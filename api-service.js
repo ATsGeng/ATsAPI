f56b3562a2c1174dcfdbf9f0693b62ae79961ab4
ahyarrr12 <mustafal.haq@digitalbdg.ac.id>
2026-04-20 16:14:28 +0700

    Refactor organization data fetching function
    Replaced fetchOrgData with getOrgData and updated URL.

--- api-service.js
+++ api-service.js
@@ -1,16 +1,8 @@
 // api-service.js
 import * as croot from "https://cdn.jsdelivr.net/gh/crootjs/lib@0.0.3/api.js";
 
-const BASE_URL = "https://api.github.com/orgs/atsgeng";
-
-// Fungsi untuk mengambil data profil organisasi
-export const fetchOrgData = (targetId) => {
-    croot.get(BASE_URL, targetId);
-};
-
-// Fungsi BARU: Mengambil daftar repositori
-export const fetchRepos = (targetId, callback) => {
-    const url = `${BASE_URL}/repos`;
-    // Kita gunakan callback agar data bisa kita modifikasi tampilannya
-    croot.get(url, targetId, callback);
+export const getOrgData = (targetId) => {
+    const url = "https://api.github.com/orgs/atsgeng";
+    // Fungsi get dari crootjs akan langsung merender JSON ke targetId
+    croot.get(url, targetId);
 };
